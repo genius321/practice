57a36ea05ea3b8e1d5018d030561933355ca396f
Konstantin Orlov <genius4666@gmail.com>
2023-05-19 01:43:32 +0300

    third commit

--- practice.go
+++ practice.go
@@ -1,7 +1,11 @@
+// Previously I had some complex logic in this
+// package, but I had to delete it due to reasons.
+// Very sad!
+
 package practice
 
 func myFunction(first, second int) int {
-	return first*second + 10
+	return first + second
 }
 
 func anotherFunction(first int) int {
