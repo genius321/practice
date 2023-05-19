99253dd77b7990e9bb96e0b113448c3308d2287f
Konstantin Orlov <genius4666@gmail.com>
2023-05-19 18:24:08 +0300

    adds secondFunction on branch second

--- practice.go
+++ practice.go
@@ -4,10 +4,16 @@
 
 package practice
 
+import "fmt"
+
 func myFunction(first, second int) int {
 	return first + second
 }
 
 func anotherFunction(first int) int {
 	return first + 1
+}
+
+func secondFunctin() {
+	fmt.Println("I am new")
 }
